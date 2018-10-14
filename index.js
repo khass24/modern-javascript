9f43835ac31883b81c63c00d6b9b935200744baf
Kyle Hassett <kylehassett72@gmail.com>
2018-10-14 13:05:39 -0500

    in class work

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("pirates have taken over the browser");
+var moment = require('moment');
+var math = require('mathjs');
+
+console.log("Woohoo");
+
+var dateTime = moment().startOf('hour').fromNow();
+console.log(dateTime);
+
+console.log(math.sqrt(4));
+
+//create a synth and connect it to the master output (your speakers)
+var synth = new Tone.Synth().toMaster();
+
+//play a middle 'C' for the duration of an 8th note
+synth.triggerAttackRelease("C4", "8n");
